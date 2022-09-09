c40bf4212a231fc16a90a37981720958cb6ae415
Яна Седова <jana.sedova2809@yandex.ru>
2022-09-09 06:22:01 +0300

    try

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -13,6 +13,6 @@ class AdminUserOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or (obj.author == request.user) 
+            or (obj.author == request.user)
             or request.user.is_staff
         )
