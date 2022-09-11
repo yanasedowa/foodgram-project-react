6fcb22fbe5ce959098473880ddb44a16e1d9ce76
Яна Седова <jana.sedova2809@yandex.ru>
2022-09-11 14:54:57 +0300

    Удаление

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,15 +1,11 @@
 import django_filters
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag
+from recipes.models import Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
-    tags = django_filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        queryset=Tag.objects.all(),
-        to_field_name='slug',
-    )
+    tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = django_filters.BooleanFilter()
     is_in_shopping_cart = django_filters.BooleanFilter()
 
